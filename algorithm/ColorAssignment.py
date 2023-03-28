bca4f3ac960c92f4c771c5b9f131e77d17d24cd4
zyrrron <1043251725@qq.com>
2023-03-27 22:36:10 -0400

    update merge stage

--- algorithm/ColorAssignment.py
+++ algorithm/ColorAssignment.py
@@ -1,5 +1,17 @@
 # Set the number of cell-cell communication as the input parameters
 # Assign coloring remarks for each edge in a dictionary and save it.
 
+def ColorAssignment():
+    # Load samples and settings
+    samples, settings = utils.loadSettings()
+
+    # Verify samples iteratively
+    for s in samples:
+
+        # Load verification result
+        G_primitive, S_bounds, primitive_only, ConstraintType, constraint, loop_free, priority, out_path, _, target_n, timestep, bio_flag = utils.loadData(s, settings)
+        VerifyResult = iof.loadVerifySolution(out_path, s)
+        CommunityNumToNodes = uf.mapCommunityToNodes(VerifyResult)
 
 
+ColorAssignment()
